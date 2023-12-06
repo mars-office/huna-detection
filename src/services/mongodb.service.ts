7ed32c0d34879a1557af84e9825fca7669792046
Alin Florin <alin@qoffice.ro>
2023-12-06 14:48:55 +0200

    -

---
+++ src/services/mongodb.service.ts
@@ -0,0 +1,10 @@
+import { MongoClient } from "mongodb";
+
+const mongoClient = new MongoClient(
+  `mongodb://admin:${process.env.MONGODB_PASSWORD}@huna-mongodb:27017/`,
+  {
+    appName: "huna-detection",
+  }
+);
+export const db = mongoClient.db("huna-detection");
+export default db;
