8b732581a73edf68ae2fa7bf34b0df7a072f8635
Alin Florin <alin@qoffice.ro>
2024-02-18 10:34:00 +0200

    -

--- src/routes/detect-route.ts
+++ src/routes/detect-route.ts
@@ -6,13 +6,22 @@ const detectRouter = Router();
 
 detectRouter.post("/api/detection/detect", (req: Request, res: Response) => {
   const parkingLotId = (req as any).user.sub;
-  const writeStream = gridFsBucket.openUploadStream(`${parkingLotId}_${v4()}.rgb565`);
+  const now = new Date().getTime();
+  const writeStream = gridFsBucket.openUploadStream(`${parkingLotId}_${now}_${v4()}.rgb565`, {
+    metadata: {
+      parkingLotId,
+      type: 'jpg',
+      createdAt: now
+    }
+  });
   writeStream.on('error', (e) => {
+    writeStream.end();
     console.error(e);
     res.status(500).send({global: ['api.detection.detect.uploadError']});
   });
   writeStream.on('close', () => {
     res.send({success: true, _id: writeStream.id});
+    
   });
   req.pipe(writeStream);
 });
